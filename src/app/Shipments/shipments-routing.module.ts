0370a0bf3cba21470866957a2988af6e90821f1b
Marcos Calderon <mcalderonig@gmail.com>
2023-03-29 11:26:22 -0600

    children roots & lazy loading

--- src/app/Shipments/shipments-routing.module.ts
+++ src/app/Shipments/shipments-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './pages/create/create.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'create',
+        component: CreateComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'home'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './Shared/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./Auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'shipments',
+    loadChildren: () => import('./Shipments/shipments.module').then(m => m.ShipmentsModule)
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
