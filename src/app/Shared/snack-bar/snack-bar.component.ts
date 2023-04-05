effc0f83155013c5da0db4915f947791e94feee6
Marcos Calderon <mcalderonig@gmail.com>
2023-04-04 23:32:08 -0600

    snack bar moved to shared

---
+++ src/app/Shared/snack-bar/snack-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { SnackBarMessage } from '../Interfaces/snackBarMessage.interface';
+
+@Component({
+  selector: 'app-snack-bar',
+  templateUrl: './snack-bar.component.html',
+  styleUrls: ['./snack-bar.component.css']
+})
+export class SnackBarComponent {
+  content: SnackBarMessage;
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: SnackBarMessage) { 
+    this.content = this.data;
+  }
+}
